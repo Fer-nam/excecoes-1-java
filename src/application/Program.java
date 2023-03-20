e501b65209087b10531264beaae6290b2eee6ca8
Fer-nam <fernandosilva348@gmail.com>
2023-03-20 00:16:21 -0300

    Solução boa

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,25 +6,26 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args)  {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.print("Número do quarto: ");
-		int number = sc.nextInt();
-		System.out.print("Data de Check-In (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Data de check-Out (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
 		
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Erro na reserva: A data de check-Out precisa ser depois da data de check-in!");
-		}
-		else {
+		try {
+			System.out.print("Número do quarto: ");
+			int number = sc.nextInt();
+			System.out.print("Data de Check-In (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Data de check-Out (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			
+			
+			
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reserva: " + reservation);
 			
@@ -35,13 +36,17 @@ public class Program {
 			System.out.print("Data de check-out (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			String error = reservation.updateDates(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Erro na reserva: " + error);
-			}
-			else {
-				System.out.println("Reserva : " + reservation);
-			}
+			reservation.updateDates(checkIn, checkOut);
+		}
+		
+		catch(ParseException e) {
+			System.out.println("Formato de data inválida!");
+		}
+		catch(DomainException e) {
+			System.out.println("Erro na reserva: "+ e.getMessage());
+		}
+		catch(RuntimeException e) {
+			System.out.println("Erro inesperado!");
 		}
 
 		sc.close();
